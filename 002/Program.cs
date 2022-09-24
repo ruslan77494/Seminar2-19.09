fa7f83522253308127d89f6d739e8f25215c8949
ruslan <rus.pro94@bk.ru>
2022-09-24 11:25:42 +0500

    .\002\

---
+++ 002/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу,
+ которая выводит случайное трёхзначное число
+  и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int GetRandomNumber(int leftRange, int rightRange) // [100-1000)
+{
+Random rnd = new Random();
+int result = rnd.Next(leftRange, rightRange); // [leftRange,rightRange)
+return result;
+}
+
+int DeleteSecondDigit(int number)
+{
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+
+return firstDigit*10 + thirdDigit;
+}
+
+int number = GetRandomNumber(100,1000);
+Console.WriteLine(number);
+
+int twoDigits = DeleteSecondDigit(number);
+Console.WriteLine(twoDigits);
+
