60825e47bc209f03f61ac0eb44d825ec623d538a
ruslan <rus.pro94@bk.ru>
2022-09-24 11:58:40 +0500

    .\004\

---
+++ 004/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу,
+ которая принимает на вход число и проверяет,
+  кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+
+
+*/
+
+
+int GetNumber() // создали функцию
+{
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+return number;
+}
+int numberA = GetNumber();
+if (numberA % 7 == 0 && numberA % 23 == 0)
+{
+Console.WriteLine("кратно");
+}
+else
+{
+Console.WriteLine("не кратно!");
+}
